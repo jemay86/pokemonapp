fb2445c7c639c193a590cb6fef55fb522476909a
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2018-11-20 13:45:57 +0100

    pokemon detail

--- pokemonapp/src/pages/pokemon-detail/pokemon-detail.ts
+++ pokemonapp/src/pages/pokemon-detail/pokemon-detail.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Pokemon } from '../../models/pokemon';
 
 /**
  * Generated class for the PokemonDetailPage page.
@@ -15,7 +16,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PokemonDetailPage {
 
+  pok: Pokemon
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.pok = navParams.get('pok');
+    
   }
 
   ionViewDidLoad() {
